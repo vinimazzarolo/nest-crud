6521e6ee15cce55c01b3851f6d41ed3a0a8a3fe9
Vinicius Mazzarolo <vinimazzarolo1@outlook.com>
2022-07-22 17:48:27 -0300

    feat: setup people api resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PeopleModule } from './people/people.module';
 
 @Module({
-  imports: [],
+  imports: [PeopleModule],
   controllers: [AppController],
   providers: [AppService],
 })
