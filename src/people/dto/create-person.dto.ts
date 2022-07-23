13f6c1ca34b35643a990cfc484516c7d4d723b29
Vinicius Mazzarolo <vinimazzarolo1@outlook.com>
2022-07-23 10:39:11 -0300

    feat: implement person validation pipe

--- src/people/dto/create-person.dto.ts
+++ src/people/dto/create-person.dto.ts
@@ -1,6 +1,33 @@
+import { Type } from 'class-transformer';
+import {
+  IsDate,
+  IsIn,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  MaxDate,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class CreatePersonDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @MinLength(11)
+  @MaxLength(14)
   identification: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @IsIn([1, 2]) // 1 = Pessoa Física, 2 = Pessoa Jurídica
   type: number;
+
+  @IsNotEmpty()
+  @Type(() => Date)
+  @IsDate()
+  @MaxDate(new Date())
   birthDate: Date;
 }
